2aaebde4e077bd35ee62757b847db6faf8da412e
Sergio Fenoll <sergio@fenoll.be>
2022-11-25 17:51:32 +0100

    feat: stick to changing accounts when impersonating + add error handling

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ app.get('/', function(_req, res) {
   res.send({ message: '👋 Hi, this is the impersonation-service 🕵' });
 });
 
-app.get('/impersonate/current', async function(req, res) {
+app.get('/who-am-i', async function(req, res) {
   const muSessionId = req.get('mu-session-id');
 
   const {
     id: sessionId,
-    impersonatedAccountId,
-    impersonatedMembershipId,
+    impersonatorAccountId,
+    impersonatorMembershipId,
   } = await getImpersonatedSession(muSessionId);
 
   const data = {
@@ -23,83 +23,128 @@ app.get('/impersonate/current', async function(req, res) {
     id: sessionId,
   }
 
-  if (impersonatedAccountId) {
+  if (impersonatorAccountId) {
     data.relationships ??= {};
     data.relationships.account = {
-      links: `/accounts/${impersonatedAccountId}`,
-      data: { type: 'accounts', id: impersonatedAccountId },
+      links: `/accounts/${impersonatorAccountId}`,
+      data: { type: 'accounts', id: impersonatorAccountId },
     }
   }
 
-  if (impersonatedMembershipId) {
+  if (impersonatorMembershipId) {
     data.relationships ??= {};
-    data.relationships.memebrship = {
-      links: `/memberships/${impersonatedMembershipId}`,
-      data: { type: 'memberships', id: impersonatedMembershipId },
+    data.relationships.membership = {
+      links: `/memberships/${impersonatorMembershipId}`,
+      data: { type: 'memberships', id: impersonatorMembershipId },
     }
   }
 
   res.send({
     links: {
-      self: '/impersonate/current',
+      self: '/who-am-i',
     },
     data,
   });
 });
 
-app.post('/impersonate/current', async function(req, res) {
-  const { impersonatedAccount, impersonatedMembership } = req.body;
+app.post('/impersonate', async function(req, res, next) {
+  const {
+    data: {
+      relationships: {
+        account: {
+          attributes: {
+            uri: impersonatedAccount,
+          }
+        },
+        membership: {
+          attributes: {
+            uri: impersonatedMembership,
+          }
+        }
+      }
+    }
+  } = req.body;
   if (!impersonatedAccount && !impersonatedMembership) {
     // Respond with an error
     res.status(400).send();
+    next({ message: `You need to pass both an account and a membership in the request body` });
     return;
   }
 
   const muSessionId = req.get('mu-session-id');
-  await setImpersonatedSession(
-    muSessionId,
-    impersonatedAccount,
-    impersonatedMembership,
-  );
 
   const {
     id: sessionId,
-    impersonatedAccountId,
-    impersonatedMembershipId,
+    impersonatorAccount,
+    impersonatorAccountId,
+    impersonatorMembership,
+    impersonatorMembershipId,
   } = await getImpersonatedSession(muSessionId);
+
+  try {
+    await setImpersonatedSession(
+      muSessionId,
+      impersonatorAccount,
+      impersonatorMembership,
+      impersonatedAccount,
+      impersonatedMembership,
+    );
+  } catch (e) {
+    if (e.httpStatus === 403) {
+      console.warn(`Session <${muSessionId}> could not write data to impersonate account <${impersonatedAccount}> with membership <${impersonatedMembership}>`)
+      next({ message: `You don't have the necessary rights to impersonate other accounts`, status: 403 });
+    } else {
+      console.warn(`Something went wrong while session <${muSessionId}> tried to impersonate account <${impersonatedAccount}> with membership <${impersonatedMembership}>`)
+      next({ message: 'Something went wrong' });
+    }
+    return;
+  }
+
+  const data = {
+    type: 'sessions',
+    id: sessionId,
+  }
+
+  if (impersonatorAccountId) {
+    data.relationships ??= {};
+    data.relationships.account = {
+      links: `/accounts/${impersonatorAccountId}`,
+      data: { type: 'accounts', id: impersonatorAccountId },
+    }
+  }
+
+  if (impersonatorMembershipId) {
+    data.relationships ??= {};
+    data.relationships.membership = {
+      links: `/memberships/${impersonatorMembershipId}`,
+      data: { type: 'memberships', id: impersonatorMembershipId },
+    }
+  }
 
   res
     .header('mu-auth-allowed-groups', 'CLEAR')
-    .status(204)
     .send({
       links: {
-        self: '/impersonate/current',
+        self: '/who-am-i',
       },
-      data: {
-        type: 'sessions',
-        id: sessionId,
-        relationships: {
-          ...(impersonatedAccountId ?
-            {
-              account: {
-                links: `/accounts/${impersonatedAccountId}`,
-                data: { type: 'accounts', id: impersonatedAccountId },
-              }
-            } : {}),
-          ...(impersonatedMembershipId ? {
-            membership: {
-              links: `/memberships/${impersonatedMembershipId}`,
-              data: { type: 'memberships', id: impersonatedMembershipId },
-            }
-          } : {}),
-        },
-      }
+      data,
     });
 });
 
 app.delete('/impersonate', async function(req, res) {
   const muSessionId = req.get('mu-session-id');
-  await deleteImpersonatedSession(muSessionId);
+  try {
+    await deleteImpersonatedSession(muSessionId);
+  } catch (e) {
+    if (e.httpStatus === 403) {
+      console.warn(`Session <${muSessionId}> could not remove impersonation data`)
+      next({ message: `You don't have the necessary rights to stop impersonating other accounts`, status: 403 });
+    } else {
+      console.warn(`Something went wrong while session <${muSessionId}> tried to stop impersonating another account`)
+      next({ message: 'Something went wrong' });
+    }
+    return;
+  }
   res.header('mu-auth-allowed-groups', 'CLEAR').status(204).send();
 });
 
