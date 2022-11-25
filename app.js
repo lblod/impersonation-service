9cd6d92420888eb8a3702c55518f33622e099515
Sergio Fenoll <sergio@fenoll.be>
2022-11-25 11:24:44 +0100

    feat: initial commit containing a base working-ish service

--- app.js
+++ app.js
@@ -1,9 +1,106 @@
-// see https://github.com/mu-semtech/mu-javascript-template for more info
+import { app, errorHandler } from 'mu';
+import {
+  getImpersonatedSession,
+  setImpersonatedSession,
+  deleteImpersonatedSession,
+} from './lib/session';
 
-import { app, query, errorHandler } from 'mu';
+app.get('/', function(_req, res) {
+  res.send({ message: '👋 Hi, this is the impersonation-service 🕵' });
+});
 
-app.get('/', function( req, res ) {
-  res.send('Hello mu-javascript-template');
-} );
+app.get('/impersonate/current', async function(req, res) {
+  const muSessionId = req.get('mu-session-id');
+
+  const {
+    id: sessionId,
+    impersonatedAccountId,
+    impersonatedMembershipId,
+  } = await getImpersonatedSession(muSessionId);
+
+  const data = {
+    type: 'sessions',
+    id: sessionId,
+  }
+
+  if (impersonatedAccountId) {
+    data.relationships ??= {};
+    data.relationships.account = {
+      links: `/accounts/${impersonatedAccountId}`,
+      data: { type: 'accounts', id: impersonatedAccountId },
+    }
+  }
+
+  if (impersonatedMembershipId) {
+    data.relationships ??= {};
+    data.relationships.memebrship = {
+      links: `/memberships/${impersonatedMembershipId}`,
+      data: { type: 'memberships', id: impersonatedMembershipId },
+    }
+  }
+
+  res.send({
+    links: {
+      self: '/impersonate/current',
+    },
+    data,
+  });
+});
+
+app.post('/impersonate/current', async function(req, res) {
+  const { impersonatedAccount, impersonatedMembership } = req.body;
+  if (!impersonatedAccount && !impersonatedMembership) {
+    // Respond with an error
+    res.status(400).send();
+    return;
+  }
+
+  const muSessionId = req.get('mu-session-id');
+  await setImpersonatedSession(
+    muSessionId,
+    impersonatedAccount,
+    impersonatedMembership,
+  );
+
+  const {
+    id: sessionId,
+    impersonatedAccountId,
+    impersonatedMembershipId,
+  } = await getImpersonatedSession(muSessionId);
+
+  res
+    .header('mu-auth-allowed-groups', 'CLEAR')
+    .status(204)
+    .send({
+      links: {
+        self: '/impersonate/current',
+      },
+      data: {
+        type: 'sessions',
+        id: sessionId,
+        relationships: {
+          ...(impersonatedAccountId ?
+            {
+              account: {
+                links: `/accounts/${impersonatedAccountId}`,
+                data: { type: 'accounts', id: impersonatedAccountId },
+              }
+            } : {}),
+          ...(impersonatedMembershipId ? {
+            membership: {
+              links: `/memberships/${impersonatedMembershipId}`,
+              data: { type: 'memberships', id: impersonatedMembershipId },
+            }
+          } : {}),
+        },
+      }
+    });
+});
+
+app.delete('/impersonate', async function(req, res) {
+  const muSessionId = req.get('mu-session-id');
+  await deleteImpersonatedSession(muSessionId);
+  res.header('mu-auth-allowed-groups', 'CLEAR').status(204).send();
+});
 
 app.use(errorHandler);
